05f2c7fa403efbaad35c865bdd7c80ce3556f8b7
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-11-21 19:16:24 -0800

    Access media URL from query args

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { page } from "$app/state";
+
+  let searchParams: URLSearchParams = page.url.searchParams;
+  let urlParam: string | null = searchParams.get("url");
+</script>
